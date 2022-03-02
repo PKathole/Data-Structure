fbfd016596f95ce5835ce76aff84e52066b55f2f
pkathole <pranaykathole93@gmail.com>
2022-03-02 12:18:20 +0530

    file added to beta

---
+++ Runner.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		list.insert(3);
+		list.insert(26);
+		list.insert(21);
+		
+		list.show();
+	}
+}
